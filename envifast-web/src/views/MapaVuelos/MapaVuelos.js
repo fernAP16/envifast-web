a517c111c75edcc7056b5a3c54ca70cec5c7bb31
e <alfredo.tarazona00@gmail.com>
2022-10-01 21:28:01 -0500

    Avioncito

--- envifast-web/src/views/MapaVuelos/MapaVuelos.js
+++ envifast-web/src/views/MapaVuelos/MapaVuelos.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './../../App';
-import {MapContainer, TileLayer, Marker} from 'react-leaflet'; // objeto principal para los mapas
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'; // objeto principal para los mapas
 import AirportLocation from '../../components/AirportLocation/AirportLocation';
 
 import 'leaflet/dist/leaflet.css'; // 
 import './MapaVuelos.css'
+// import '../../assets/icons/airport-01.svg'
+import L from "leaflet"
 
+
+function getIcon(_iconSize){
+    return L.icon({
+        // iconUrl : require("../../assets/icons/airport-01.svg"),
+        // iconRetinaUrl: require("../../assets/icons/airport-01.svg"),
+        iconUrl : require("../../assets/icons/aeropuerto.png"),
+        iconRetinaUrl: require("../../assets/icons/aeropuerto.png"),
+        iconSize : _iconSize
+    })
+}
 
 // const MapaVuelos  = (props) => {
     const MapaVuelos  = () => {
-    // hooks
-    // hooks
-    // center es una propiedad que indica la ubicacion inicial en el mapa. Recibe en un objeto la latitud y longitud
-    // zoom: tambien es una propiedad para ver que tan cerca o lejos queremos ver nuestro mapa
-    // return <div>
-    //         <h3>Mapa de vuelos</h3>
-        //    return <MapContainer center = {{lat: '28.058522', lng: '-20.591226'}} zoom = {2.8}>
-        //         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'>
-
-        //         </TileLayer>
-        //     </MapContainer>
-        //     <Marker position={{lat:-12.0219,lng:-77.1112}}></Marker>
+    
         const AirportMarket = () => {
             return (
                 <Marker position={{lat:-12.0219,lng:-77.1112}} img={<AirportLocation/>}></Marker>
@@ -37,7 +38,9 @@ import './MapaVuelos.css'
                     minZoom = {2.0}
                     maxZoom = {18.0}
                 >
-                    <AirportMarket/>
+                    <Marker position={["28.058522", "-20.591226"]} icon = {getIcon(20)}> 
+                        <Popup> A pretty CSS3 group</Popup>
+                    </Marker>
                     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'></TileLayer>
                 </MapContainer>
                 
