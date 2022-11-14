03fc1e7458b7523457966af998245f1c27683d64
Fernando Arias <82820449+fernAP16@users.noreply.github.com>
2022-11-14 11:37:07 -0500

    Vista del login añadida

--- envifast-web/src/views/Login/Login.js
+++ envifast-web/src/views/Login/Login.js
@@ -1,22 +1,33 @@
-import { Button, Icon } from "@mui/material";
+import { Box, Button, Grid, Icon, TextField, Typography } from "@mui/material";
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
 import * as ROUTES from '../../routes/routes.js'
 import ArrowIcon from '../../assets/icons/arrow-right.png'
+import packages from '../../assets/icons/packages.png'
 
 const Login  = (props) => {   
     let navigate = useNavigate();
 
     return (
-        <div>
-            <h3>Iniciar sesion</h3>
-            <Button 
-                className="buttonLogin"
-                onClick={() => {navigate(ROUTES.ENVIOS);}}
-                endIcon={<img src={ArrowIcon} width="22px" />}
-            >
-                INGRESAR
-            </Button>
+        <div> 
+            <Grid>
+                <Typography className='view-title' align='center'>Envíos de paquetes RedEx</Typography>
+                <img src={packages} className='view-image'/>
+            </Grid> 
+            <Grid className='container-login'>
+                <Typography className='login-title' align='center'>Iniciar sesión</Typography>
+                <TextField className='login-textfield-email' label='Correo electrónico' variant='standard' />
+                <TextField className='login-textfield-password' label='Contraseña' variant='standard' />
+                <Box textAlign='center'>
+                    <Button 
+                        className="login-button"
+                        onClick={() => {navigate(ROUTES.ENVIOS);}}
+                        endIcon={<img src={ArrowIcon} width="22px" />}
+                    >
+                        INGRESAR
+                    </Button>
+                </Box>
+            </Grid>
         </div>
     );
 }
