de412007bd53841ec44d2175ab4fed38fdb4d18f
Fernando Arias <82820449+fernAP16@users.noreply.github.com>
2022-11-28 17:02:40 -0500

    Planificador sin service

--- envifast-web/src/App.js
+++ envifast-web/src/App.js
@@ -5,6 +5,16 @@ import { CssBaseline } from '@mui/material';
 
 
 function App() {
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      // planificador
+    }, 600000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [])
+  
   return (
     <StyledEngineProvider injectFirst>   
         <CssBaseline/>
